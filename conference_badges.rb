beb1d49973bad1aa87645792a1920680fd3fa5f8
Anuj Panchal <anujpan1@gmail.com>
2018-09-21 02:45:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator
-  puts assign_rooms
+  batch_badge_creator(attendees).each do |name|
+    puts name
+  end
+  assign_rooms(attendees).each do |name|
+    puts name
+  end
 end
